9ffb71a95d2c61d876b5cbedb7d39da988a1a38c
MilanVlaski <milan.vlaski321@gmail.com>
2023-09-30 12:58:11 +0200

    timer starts at 0

--- test/test/TestTimer.java
+++ test/test/TestTimer.java
@@ -4,11 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import timer.Timer;
+
 class TestTimer {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void shouldBeZeroOnStartup() {
+		Timer timer = new Timer();
+		assertEquals(0, timer.time());
 	}
 
 }
