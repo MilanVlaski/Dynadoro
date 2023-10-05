12c290da7875a69e682e789596c9b2dd071c4d49
MilanVlaski <milan.vlaski321@gmail.com>
2023-10-05 16:07:16 +0200

    all tests passing after taking out a lot of stuff

--- src/timer/Timer.java
+++ src/timer/Timer.java
@@ -1,61 +1,34 @@
 package timer;
 
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+
+import timer.state.Idle;
 import timer.state.TimerStateI;
 import timer.state.Working;
 
 public class Timer {
 
-	public enum TimerState {
-		WORKING,
-		TAKING_BREAK,
-		// PAUSED,
-		IDLE;
-	}
-
 	private final Clock clock;
-	
-	private TimerState timerState = TimerState.IDLE;
-	private int startTime;
-	private int pauseTime;
-	private static final int BREAK_TIME_FACTOR = 5;
+	private TimerStateI state = new Idle(this);
 
 	public Timer(Clock clock) {
 		this.clock = clock;
 	}
 
 	public int time() {
-
-		int currentTime = currentTime();
-		int elapsedTime = currentTime - startTime;
-
-		switch (timerState) {
-		case WORKING:
-			return elapsedTime;
-		case TAKING_BREAK:
-			int breakRemaining = elapsedTime / BREAK_TIME_FACTOR - (currentTime - pauseTime);
-			if (breakRemaining > 0)
-				return breakRemaining;
-		default:
-			return 0;
-		}
+		return state.displayedTime(clock.currentTimeSeconds());
 	}
 
 	public void begin() {
-		startTime = currentTime();
-		timerState = TimerState.WORKING;
-	}
-
-	public int currentTime() {
-		return clock.currentTimeSeconds();
+		state.begin(clock.currentTimeSeconds());
 	}
 
 	public void takeBreak() {
-		timerState = TimerState.TAKING_BREAK;
-		pauseTime = currentTime();
+		state.takeBreak(clock.currentTimeSeconds());
 	}
 
-	public void changeState(TimerStateI state) {
-		
+	public void changeState(TimerStateI newState) {
+		this.state = newState;
 	}
 
 }
