615a53712ca90951f95852c0be586c229d6308a0
MilanVlaski <milan.vlaski321@gmail.com>
2023-11-06 14:54:03 +0100

    refactoring GUI

--- src/display/swing/MainFrame.java
+++ src/display/swing/MainFrame.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.awt.event.ActionListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -13,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.border.BevelBorder;
 
 import display.ConsoleDisplay;
 import timer.Timer;
@@ -22,63 +24,70 @@ public class MainFrame extends JFrame {
 	private final JPanel mainPanel = new JPanel(true);
 	private final JPanel controlPanel = new JPanel();
 	private final JLabel clock = new JLabel();
-	private final JPanel clockPanel;
+	private JPanel clockPanel;
 
-	public static final Font BUTTON_FONT = new Font("Loto", Font.PLAIN, 30);
-	public static final Font CLOCK_FONT = new Font("Loto", Font.PLAIN, 50);
+	public static final Font BUTTON = new Font("Loto", Font.PLAIN, 30);
+	public static final Font CLOCK = new Font("Loto", Font.PLAIN, 50);
 
+	private static final Color WORK = new Color(126, 223, 202);;
 	public Timer timer;
 
 	public MainFrame() {
 
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		mainPanelSetup();
 
-		mainPanel.add(Box.createVerticalGlue());
+		locationSetup();
 
-		clock.setFont(CLOCK_FONT);
-		clockPanel = new JPanel();
-		clockPanel.add(clock);
-		mainPanel.add(clockPanel);
-
-		mainPanel.add(controlPanel);
-		mainPanel.add(Box.createVerticalGlue());
-
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int x = screenSize.width - getWidth() - 400;
-		int y = 100;
-		setLocation(x, y);
+		setSize(310, 290);
 
 		setLayout(new BorderLayout());
 		add(mainPanel, BorderLayout.CENTER);
 
-		setSize(330, 270);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+
+	private void mainPanelSetup() {
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+		mainPanel.add(Box.createVerticalGlue());
+
+		clock.setFont(CLOCK);
+		clockPanel = new JPanel();
+		clockPanel.add(clock);
+
+		mainPanel.add(clockPanel);
+		mainPanel.add(controlPanel);
+
+		mainPanel.add(Box.createVerticalGlue());
+	}
+
+	private void locationSetup() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = screenSize.width - getWidth() - 400;
+		int y = 100;
+		setLocation(x, y);
 	}
 
 	private void paintBackground(Color color) {
 		mainPanel.setBackground(color);
 		clockPanel.setBackground(color);
 		controlPanel.setBackground(color);
+		clock.setForeground(new Color(color.getRed() / 10, color.getGreen() / 10,
+				color.getBlue() / 10));
 	}
 
-	public void setTime(int displayedTime) {
-		clock.setText(ConsoleDisplay.displayedTime(displayedTime));
+	public void setTime(int time) {
+		clock.setText(ConsoleDisplay.displayedTime(time));
 	}
 
 	public void showStartButton() {
-		JButton startButton = new JButton("Start");
-		startButton.setFont(BUTTON_FONT);
-		startButton.setPreferredSize(new Dimension(120, 70));
-		startButton.setFocusPainted(false);
-		startButton.addActionListener((e) -> timer.begin());
-
-		controlPanel.add(startButton);
+		showButton("Start", 120, 70, (e) -> timer.begin());
 	}
 
 	public void showPauseButton() {
 		JButton pauseButton = new JButton("| |");
-		pauseButton.setFont(BUTTON_FONT);
+		pauseButton.setFont(BUTTON);
 		pauseButton.setPreferredSize(new Dimension(70, 70));
 		pauseButton.setFocusPainted(false);
 		pauseButton.addActionListener((e) -> timer.pause());
@@ -88,8 +97,8 @@ public class MainFrame extends JFrame {
 
 	public void showBreakButton() {
 		JButton breakButton = new JButton("Take break");
-		breakButton.setFont(BUTTON_FONT);
-		breakButton.setPreferredSize(new Dimension(200, 70));
+		breakButton.setFont(BUTTON);
+		breakButton.setPreferredSize(new Dimension(182, 70));
 		breakButton.setFocusPainted(false);
 		breakButton.addActionListener((e) -> timer.takeBreak());
 
@@ -98,7 +107,7 @@ public class MainFrame extends JFrame {
 
 	private void showResumeButton() {
 		JButton resumeButton = new JButton(">");
-		resumeButton.setFont(BUTTON_FONT);
+		resumeButton.setFont(BUTTON);
 		resumeButton.setPreferredSize(new Dimension(70, 70));
 		resumeButton.setFocusPainted(false);
 		resumeButton.addActionListener((e) -> timer.resume());
@@ -118,8 +127,7 @@ public class MainFrame extends JFrame {
 			clearControls();
 			showPauseButton();
 			showBreakButton();
-			paintBackground(new Color(126, 223, 202));
-			clock.setForeground(new Color(13, 22, 20));
+			paintBackground(WORK);
 		});
 	}
 
@@ -166,6 +174,16 @@ public class MainFrame extends JFrame {
 			showBreakButton();
 			paintBackground(new Color(134, 215, 197));
 		});
+	}
+
+	private void showButton(String text, int width, int height, ActionListener actionListener) {
+		JButton startButton = new JButton(text);
+		startButton.setFont(BUTTON);
+		startButton.setPreferredSize(new Dimension(width, height));
+		startButton.setFocusPainted(false);
+		startButton.addActionListener(actionListener);
+
+		controlPanel.add(startButton);
 	}
 
 }
