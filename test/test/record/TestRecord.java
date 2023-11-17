5e20d51bc81b4d9799019ddfb117af0255890ade
MilanVlaski <milan.vlaski321@gmail.com>
2023-11-17 18:33:47 +0100

    made all record tests pass

--- test/test/record/TestRecord.java
+++ test/test/record/TestRecord.java
@@ -1,6 +1,7 @@
 package test.record;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static test.TestTimer.REST_DURATION;
 import static test.TestTimer.WORK_DURATION;
 
 import java.time.LocalDateTime;
@@ -106,7 +107,7 @@ class TestRecord
 	}
 
 	@Test
-	void shouldRecordWorking_ThenResting()
+	void shouldRecordWorking_AndStartOfResting()
 	{
 		timer.begin(moment.current());
 		timer.rest(moment.afterMinutes(WORK_DURATION));
@@ -116,60 +117,49 @@ class TestRecord
 		assertEquals("2023-11-07, Tuesday, Working, 15:40, 16:05\n",
 		        fakeHistory.read());
 	}
-//
-//	@Test
-//	void shouldRecordWorkAndThenRest() {
-//		when(mockClock.currentTimeSeconds())
-//				.thenReturn(moment.current(), moment.after(TWENTY_FIVE * 60),
-//							moment.after(REST_DURATION * 60));
-//
-//		timer.begin();
-//		timer.rest();
-//		timer.reset();
-//
-//		String expected = "2023-11-07, Tuesday, Working, 15:40, 16:05\n"
-//				+ "2023-11-07, Tuesday, Resting, 16:05, 16:10\n";
-//		assertEquals(expected, record.toString());
-//		assertEquals(expected, fakeHistory.read());
-//	}
-//
-//	@Test
-//	void shouldRecordWorkAndThenRest_IfRestIsntCancelled() {
-//		when(mockClock.currentTimeSeconds())
-//				.thenReturn(moment.current(), moment.after(TWENTY_FIVE * 60),
-//							moment.after(8 * 60));
-//
-//		timer.begin();
-//		timer.rest();
-//		timer.reset();
-//
-//		assertEquals("2023-11-07, Tuesday, Working, 15:40, 16:05\n"
-//				+ "2023-11-07, Tuesday, Resting, 16:05, 16:13\n", record.toString());
-//	}
-//
-//	@Test
-//	void shouldRecordAFinishedWorkSession_BecauseOfPause() {
-//		when(mockClock.currentTimeSeconds())
-//				.thenReturn(moment.current(), moment.after(3 * 60));
-//
-//		timer.begin();
-//		timer.pause();
-//		
-//		assertEquals("2023-11-07, Tuesday, Working, 15:40, 15:43\n", record.toString());
-//	}
-//
-//	@Test
-//	void shouldRecordWorkTwice_AfterPartuseAndResume() {
-//		when(mockClock.currentTimeSeconds())
-//				.thenReturn(moment.current(), moment.after(3 * 60),
-//						moment.after(2 * 60));
-//		
-//		timer.begin();
-//		timer.pause();
-//		timer.resume();
-//		
-//		assertEquals("2023-11-07, Tuesday, Working, 15:40, 15:43\n"
-//				+ "2023-11-07, Tuesday, Working, 15:45, unknown\n", record.toString());
-//	}
+
+	@Test
+	void shouldRecordWork_AndRest()
+	{
+		timer.begin(moment.current());
+		timer.rest(moment.afterMinutes(WORK_DURATION));
+		timer.reset(moment.afterMinutes(REST_DURATION));
+
+		assertEquals("2023-11-07, Tuesday, Idle, 16:10, unknown", record.toString());
+		assertEquals("2023-11-07, Tuesday, Working, 15:40, 16:05\n"
+		        + "2023-11-07, Tuesday, Resting, 16:05, 16:10\n", fakeHistory.read());
+	}
+
+	@Test
+	void shouldRecordWorkAndRest_EvenIfRestingForLongerThanAllowed()
+	{
+		timer.begin(moment.current());
+		timer.rest(moment.afterMinutes(WORK_DURATION));
+		timer.reset(moment.afterMinutes(60));
+
+		assertEquals("2023-11-07, Tuesday, Working, 15:40, 16:05\n"
+		        + "2023-11-07, Tuesday, Resting, 16:05, 17:05\n", fakeHistory.read());
+	}
+
+	@Test
+	void shouldRecordAFinishedWorkSession_BecauseOfPause()
+	{
+		timer.begin(moment.current());
+		timer.pause(moment.afterMinutes(3));
+
+		assertEquals("2023-11-07, Tuesday, Working, 15:40, 15:43\n", fakeHistory.read());
+	}
+
+	@Test
+	void shouldRecordWorkTwice_BeforeAndAfterPause()
+	{
+		timer.begin(moment.current());
+		timer.pause(moment.afterMinutes(2));
+		timer.resume(moment.afterMinutes(3));
+		timer.reset(moment.afterMinutes(4));
+
+		assertEquals("2023-11-07, Tuesday, Working, 15:40, 15:42\n"
+		        + "2023-11-07, Tuesday, Working, 15:45, 15:49\n", fakeHistory.read());
+	}
 
 }
