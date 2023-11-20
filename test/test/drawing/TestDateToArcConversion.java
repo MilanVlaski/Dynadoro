73f0d8e3e990f80f091a0f52f9bed6f13d828f39
MilanVlaski <milan.vlaski321@gmail.com>
2023-11-20 21:06:59 +0100

    formatting

--- test/test/drawing/TestDateToArcConversion.java
+++ test/test/drawing/TestDateToArcConversion.java
@@ -12,63 +12,63 @@ class TestDateToArcConversion
 {
 
 	@Test
-	void shouldConvertMidnight_ToNinety()
+	void ConvertMidnight_ToNinety()
 	{
 		LocalTime midnight = LocalTime.of(0, 0);
 		assertEquals(90, Day.timeToDegrees(midnight));
 	}
 
 	@Test
-	void shouldConvertOneAM_ToSixty()
+	void ConvertOneAM_ToSixty()
 	{
 		LocalTime oneAM = LocalTime.of(1, 0);
 		assertEquals(60, Day.timeToDegrees(oneAM));
 	}
 
 	@Test
-	void shouldConvertFiveAM_ToMinusSixty()
+	void ConvertFiveAM_ToMinusSixty()
 	{
 		LocalTime fiveAM = LocalTime.of(5, 0);
 		assertEquals(-60, Day.timeToDegrees(fiveAM));
 	}
 
 	@Test
-	void shouldConvertFivePM_ToMinusSixty()
+	void ConvertFivePM_ToMinusSixty()
 	{
 		LocalTime fivePM = LocalTime.of(17, 0);
 		assertEquals(-60, Day.timeToDegrees(fivePM));
 	}
 
 	@Test
-	void shouldConvertOneThirtyAM_ToFortyFive()
+	void ConvertOneThirtyAM_ToFortyFive()
 	{
 		LocalTime oneThirty = LocalTime.of(1, 30);
 		assertEquals(45, Day.timeToDegrees(oneThirty));
 	}
 
 	@Test
-	void shouldConvertTenMinutes_ToEightyFive()
+	void ConvertTenMinutes_ToEightyFive()
 	{
 		LocalTime oneThirty = LocalTime.of(0, 10);
 		assertEquals(85, Day.timeToDegrees(oneThirty));
 	}
 
 	@Test
-	void shouldConvertFiveMinutes_ToEightySeven()
+	void ConvertFiveMinutes_ToEightySeven()
 	{
 		LocalTime oneThirty = LocalTime.of(0, 5);
 		assertEquals(88, Day.timeToDegrees(oneThirty));
 	}
 
 	@Test
-	void shouldConvertTwoMinutes_ToOneEightyNine()
+	void ConvertTwoMinutes_ToEightyNine()
 	{
 		LocalTime oneThirty = LocalTime.of(0, 2);
 		assertEquals(89, Day.timeToDegrees(oneThirty));
 	}
 
 	@Test
-	void shouldConvertOneMinute_ToNinety()
+	void ConvertOneMinute_ToNinety()
 	{
 		LocalTime oneThirty = LocalTime.of(0, 1);
 		assertEquals(90, Day.timeToDegrees(oneThirty));
